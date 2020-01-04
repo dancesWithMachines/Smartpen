76c185cd04660e43143fcbfd62f024f2592590b9
Timax <timax2@wp.pl>
2020-01-05 00:51:30 +0100

    Uploading visualiser

---
+++ Visualiser/Visualiser/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Visualiser
+{
+    partial class cursor
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(cursor));
+            this.selectPort = new System.Windows.Forms.GroupBox();
+            this.selectPortBox = new System.Windows.Forms.ComboBox();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dButton = new System.Windows.Forms.Button();
+            this.eButton = new System.Windows.Forms.Button();
+            this.cursorek = new System.Windows.Forms.PictureBox();
+            this.selectPort.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.cursorek)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // selectPort
+            // 
+            this.selectPort.BackColor = System.Drawing.Color.Transparent;
+            this.selectPort.Controls.Add(this.connectButton);
+            this.selectPort.Controls.Add(this.selectPortBox);
+            resources.ApplyResources(this.selectPort, "selectPort");
+            this.selectPort.Name = "selectPort";
+            this.selectPort.TabStop = false;
+            // 
+            // selectPortBox
+            // 
+            this.selectPortBox.FormattingEnabled = true;
+            resources.ApplyResources(this.selectPortBox, "selectPortBox");
+            this.selectPortBox.Name = "selectPortBox";
+            // 
+            // connectButton
+            // 
+            resources.ApplyResources(this.connectButton, "connectButton");
+            this.connectButton.Name = "connectButton";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.outputTextBox);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.BackColor = System.Drawing.SystemColors.InfoText;
+            this.outputTextBox.ForeColor = System.Drawing.Color.Lime;
+            resources.ApplyResources(this.outputTextBox, "outputTextBox");
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ReadOnly = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.eButton);
+            this.groupBox2.Controls.Add(this.dButton);
+            resources.ApplyResources(this.groupBox2, "groupBox2");
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.TabStop = false;
+            // 
+            // dButton
+            // 
+            resources.ApplyResources(this.dButton, "dButton");
+            this.dButton.Name = "dButton";
+            this.dButton.UseVisualStyleBackColor = true;
+            this.dButton.Click += new System.EventHandler(this.dButton_Click);
+            // 
+            // eButton
+            // 
+            resources.ApplyResources(this.eButton, "eButton");
+            this.eButton.Name = "eButton";
+            this.eButton.UseVisualStyleBackColor = true;
+            this.eButton.Click += new System.EventHandler(this.eButton_Click);
+            // 
+            // cursorek
+            // 
+            this.cursorek.Image = global::Visualiser.Properties.Resources.kursor3;
+            resources.ApplyResources(this.cursorek, "cursorek");
+            this.cursorek.Name = "cursorek";
+            this.cursorek.TabStop = false;
+            // 
+            // cursor
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Visualiser.Properties.Resources.siatka;
+            this.Controls.Add(this.cursorek);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.selectPort);
+            this.DoubleBuffered = true;
+            this.Name = "cursor";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.cursor_Load);
+            this.selectPort.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.cursorek)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox selectPort;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.ComboBox selectPortBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button eButton;
+        private System.Windows.Forms.Button dButton;
+        private System.Windows.Forms.PictureBox cursorek;
+    }
+}
+
---
+++ Visualiser/Visualiser/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.IO;
+
+namespace Visualiser
+{
+    public partial class cursor : Form
+    {
+        SerialPort choosenPort;
+        String[] portsAvailable;
+        bool isConnected = false;
+        Thread Hilo;
+        int posX, posY;
+        int x;
+        int y;
+        string i;
+        int sumX, sumY;
+
+        public cursor()
+        {
+            InitializeComponent();
+            setPorts();
+            posX = cursorek.Location.X;
+            posY = cursorek.Location.Y;
+        }
+
+        private void cursor_Load(object sender, EventArgs e)
+        {
+            //A Thread to listen forever the serial port
+            Hilo = new Thread(ListenSerial);
+            Hilo.Start();
+        }
+
+        private void ListenSerial()
+        {
+            while (true)
+            {
+                try
+                {
+                    //read to data from arduino
+                    string lastRead = choosenPort.ReadLine();
+                    if (lastRead.StartsWith("X"))
+                    {
+                        int s1 = lastRead.IndexOf("X:");
+                        int s2 = lastRead.IndexOf("/Z:");
+                        int s3 = lastRead.IndexOf("/I:");
+                        int s4 = lastRead.IndexOf("//");
+                        //x = lastRead.Substring(s1+2, s2 - s1 - 2);
+                        y = System.Convert.ToInt32(lastRead.Substring(s2+3, s3 - s2 - 3));
+                        i = lastRead.Substring(s3+3, s4 - s3 - 3);
+                        x = System.Convert.ToInt32(lastRead.Substring(s1 + 2, s2 - s1 - 2));
+                        if (sumX+x > -400 || sumX+x <400)
+                            sumX += x;
+                        if (sumY+y > -300 || sumY +y < 300)
+                            sumY += y;
+                    }
+                    //write the data in something textbox
+                    outputTextBox.Invoke(new MethodInvoker(
+                        delegate
+                        {
+                            outputTextBox.Text = lastRead;
+                        }
+                        ));
+                    cursorek.Invoke(new MethodInvoker(
+                        delegate
+                        {
+                            cursorek.Location = new Point(posX+x, posY+y);
+                        }
+                        ));
+
+                }
+                catch { }
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //when the form will be closed this line close the serial port
+            if (isConnected)
+                choosenPort.Close();
+            Hilo.Abort();
+        }
+
+        private void setPorts()
+        {
+            portsAvailable = SerialPort.GetPortNames();
+            foreach (string port in portsAvailable)
+            {
+                selectPortBox.Items.Add(port);
+            }
+        }
+        private void setStatus(string newStatus)
+        {
+            outputTextBox.Text = newStatus;
+        }
+        private void setControlls(bool isOn)
+        {            
+            selectPortBox.Enabled = !isOn;
+            connectButton.Enabled = !isOn;
+        }
+
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            string selectedPort = selectPortBox.GetItemText(selectPortBox.SelectedItem);
+            if (!string.IsNullOrEmpty(selectPortBox.Text))
+            {
+                setStatus("Port selected");
+                choosenPort = new SerialPort(selectedPort, 115200, Parity.None, 8, StopBits.One);
+                if (!isConnected)
+                {
+                    try
+                    {
+                        choosenPort.Open();
+                        setStatus("Enabling");
+                    }
+                    catch
+                    {
+                        setStatus("Port busy");
+                    }
+                }
+
+                if (choosenPort.IsOpen)
+                {
+                    choosenPort.ReadTimeout = 10000;
+                    try
+                    {
+                        isConnected = true;
+                        setControlls(isConnected);
+                    }
+                    catch (TimeoutException)
+                    {
+                        setStatus("No connection");
+                        choosenPort.Close();
+                    }
+                }
+            }
+        }
+
+        private void eButton_Click(object sender, EventArgs e)
+        {
+            if (isConnected)
+            {
+                choosenPort.WriteLine("e");
+            }
+        }
+
+        private void dButton_Click(object sender, EventArgs e)
+        {
+            if (isConnected)
+            {
+                choosenPort.WriteLine("d");
+            }
+        }
+
+    }
+}
